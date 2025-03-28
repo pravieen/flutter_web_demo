5242b12d886fc363749c26bb4def09dee9e708a7
pravieen <pravieen25602@gmail.com>
2025-03-28 14:01:50 +0530

    new_file

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3364604294"
+    serviceWorkerVersion: "2640933356"
   }
 });
